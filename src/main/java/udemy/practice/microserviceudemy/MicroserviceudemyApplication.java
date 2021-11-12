87889bc2a3d9d5f52556d5d1ea0471fb2a0386e3
Pristyn Care <himanshu.jain@pristyncare.com>
2021-11-12 16:40:27 +0530

    first commit

---
+++ src/main/java/udemy/practice/microserviceudemy/MicroserviceudemyApplication.java
@@ -0,0 +1,13 @@
+package udemy.practice.microserviceudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceudemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceudemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/udemy/practice/microserviceudemy/MicroserviceudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.practice.microserviceudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
